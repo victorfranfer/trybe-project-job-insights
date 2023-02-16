e4e8be10e93df41062df59d21937dd19dd93dc08
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-16 17:42:06 -0300

    req3:get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,11 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    data = read(path)
+    industries = set()
+    for industry in data:
+        industries.add(industry['industry'])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
