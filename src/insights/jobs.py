5bc48e546c8a7f86188e809694d4a16a887ee29b
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-16 19:32:11 -0300

    req6:filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -62,4 +62,8 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    jobs_list = []
+    for job in jobs:
+        if job['job_type'] == job_type:
+            jobs_list.append(job)
+    return jobs_list
