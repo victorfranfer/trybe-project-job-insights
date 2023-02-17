d4865fd9ed2cca937e4a93bedf477f75548639a7
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-17 15:33:07 -0300

    req12:test_sorting:flake8_fix

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -26,7 +26,7 @@ def test_sort_by_criteria():
             "min_salary": "4000",
             "max_salary": "8000",
             "date_posted": "2023-01-31",
-        }, 
+        },
     ]
 
     sort_by(jobs, "max_salary")
@@ -42,7 +42,7 @@ def test_sort_by_criteria():
         "title": "Front end developer",
         "min_salary": "1000",
         "max_salary": "2000",
-        "date_posted": "2021-03-16",      
+        "date_posted": "2021-03-16",
     }
 
     sort_by(jobs, "date_posted")
