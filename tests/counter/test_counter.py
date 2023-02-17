880779822c3a120fbfeb68344e2ddff2a7ba42e2
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-17 14:54:53 -0300

    req10:test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    PYTHON_STRING = "python"
+    PYTHON_OCURRENCES = 1639
+    python_counter = count_ocurrences("data/jobs.csv", PYTHON_STRING)
+    assert python_counter == PYTHON_OCURRENCES
+
+    JS_STRING = "javascript"
+    JS_OCURRENCES = 122
+    js_counter = count_ocurrences("data/jobs.csv", JS_STRING)
+    assert js_counter == JS_OCURRENCES
