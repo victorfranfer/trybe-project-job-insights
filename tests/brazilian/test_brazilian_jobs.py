cb6cbb6a644ab19cbf7787213361ac24c0015793
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-17 15:12:19 -0300

    req11:test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert brazilian_jobs[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
